2d010c339052a18aba12b6c30b7aae6901fa7d2b
Nelson-help <1119nelsonl@gmail.com>
2025-07-04 20:33:03 +0800

    Improved image change

--- ImageChange.py
+++ ImageChange.py
@@ -8,47 +8,30 @@ class ImagesDisplay(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("ImageChange")
-        self.image = QLabel(self)
-
-        self.cloud = QPixmap(os.path.join("Image_Examplar", "JPG_1.jpg")).scaledToHeight(900)
-        self.plant = QPixmap(os.path.join("Image_Examplar", "JPG_2.jpg")).scaledToHeight(900)
-        self.sun = QPixmap(os.path.join("Image_Examplar", "JPG_3.jpg")).scaledToHeight(900)
-
-        self.showing_jpg = True
         
-        # Intiate with cloud
-        self.image.setPixmap(self.cloud)
-        self.image.setFixedSize(self.cloud.size())
-        self.setFixedSize(self.cloud.width(), self.cloud.height())
-
-        self.cur_pic = "cloud"
-
+        # Create Widgets
+        self.image = QLabel(self)
         self.btn = QPushButton("CLICK TO CHANGE!!", self)
+
+        self.imagePaths = [os.path.join("Image_examplar", f"JPG_{i+1}.jpg") for i in range(3)]
+        self.imagePixmaps = [QPixmap(path).scaledToHeight(900) for path in self.imagePaths]
+        self.imageIndex = -1 # imageIndex starts at -1 to show the "zeroth" image
+        self.NextImage()
+
         self.btn.setStyleSheet("background-color: rgba(0, 0, 0, 50)")
-        self.btn.setGeometry(0, 0, self.cloud.width(), 100)
-        self.btn.clicked.connect(self.OnBtnClicked)
+        self.btn.setGeometry(0, 0, self.imagePixmaps[self.imageIndex].width(), 100)
+        self.btn.clicked.connect(self.NextImage)
         
     @pyqtSlot()
-    def OnBtnClicked(self):
-        if self.cur_pic == "cloud":
-            self.image.setPixmap(self.plant)
-            self.image.setFixedSize(self.plant.size())
-            self.btn.resize(self.plant.width(), 100)
-            self.setFixedSize(self.plant.width(), self.plant.height())
-            self.cur_pic = "plant"
-        elif self.cur_pic == "plant":
-            self.image.setPixmap(self.sun)
-            self.image.setFixedSize(self.sun.size())
-            self.btn.resize(self.sun.width(), 100)
-            self.setFixedSize(self.sun.width(), self.sun.height())
-            self.cur_pic = "sun"
-        elif self.cur_pic == "sun":
-            self.image.setPixmap(self.cloud)
-            self.image.setFixedSize(self.cloud.size())
-            self.btn.resize(self.cloud.width(), 100)
-            self.setFixedSize(self.cloud.width(), self.cloud.height())
-            self.cur_pic = "cloud"
-            
+    def NextImage(self):
+        # Cycle through images (add one to move through the list, remainder to cycle)
+        self.imageIndex = (self.imageIndex + 1) % len(self.imagePixmaps)
+        
+        self.image.setPixmap(self.imagePixmaps[self.imageIndex])
+        self.image.setFixedSize(self.imagePixmaps[self.imageIndex].size())
+        self.btn.resize(self.imagePixmaps[self.imageIndex].width(), 100)
+        self.setFixedSize(self.imagePixmaps[self.imageIndex].size())
+
 app = QApplication([])
 
 window = ImagesDisplay()
